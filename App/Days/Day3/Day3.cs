56e4a98852c791cb89f9b225706dc382ebddb056
Gabriel <25264787+deployHuman@users.noreply.github.com>
2023-12-03 13:34:15 +0100

    Solved Day 3

--- App/Days/Day3/Day3.cs
+++ App/Days/Day3/Day3.cs
@@ -9,12 +9,27 @@ namespace AdventOfCode2023.App.Days
 
         public List<NumberGroup> AllNumberGroups = new();
 
+        public Dictionary<int, Dictionary<int, List<NumberGroup>>> AllGears = new();
+
         public Day3()
         {
             ReadInProblem();
             ReadInAsMap();
             SetAllNumbersFromLines();
         }
+
+        /// <summary>
+        /// A coordinate on the grid
+        /// X is horizontal, Y is vertical
+        /// All cooridnates being from the top left and go down and right
+        /// </summary>
+        public class Coordinate
+        {
+            public int X;
+            public int Y;
+            public string Content = "";
+        }
+
         private void ReadInAsMap()
         {
             int x = 0;
@@ -64,53 +79,10 @@ namespace AdventOfCode2023.App.Days
         }
 
 
-        public override void Part1()
-        {
-            int TotalSumOfParts = 0;
-            foreach (NumberGroup numberGroup in AllNumberGroups)
-            {
-                string Content = GetContentFromAllAdjecentCoords(numberGroup);
-                bool IsPart = DoesItContainAPart(Content);
-                Functions.DebugPrint("Number " + numberGroup.Number + " at " + numberGroup.StartingCoordinate.X + "," + numberGroup.StartingCoordinate.Y + " has content: " + Content + " Is there a Part here? : " + IsPart.ToString());
-                Functions.DebugPrint("###\n");
-                if (IsPart)
-                {
-                    TotalSumOfParts += numberGroup.Number;
-                }
-            }
-            Functions.PrintResult(TotalSumOfParts.ToString(), 3, 1);
-            //409423 is too low
-            //556057 is right!
-        }
 
-        private bool DoesItContainAPart(string AdjacenContent)
-        {
-            AdjacenContent = Regex.Replace(AdjacenContent, @"\d+|\.+| +", "").Trim();
-            Functions.DebugPrint("Filtered AdjacenContent : '" + AdjacenContent + "' Length: " + AdjacenContent.Length);
-            return !(AdjacenContent.Length == 0);
-        }
 
-        public string GetContentFromAllAdjecentCoords(NumberGroup numberGroup)
-        {
-            string content = "";
-            foreach (Coordinate coord in numberGroup.AdjacentCoords)
-            {
-                content += GetContentFromCoord(coord);
-            }
-            return content;
-        }
 
-        public string? GetContentFromCoord(Coordinate coord)
-        {
-            if (TheWholeMap.ContainsKey(coord.X))
-            {
-                if (TheWholeMap[coord.X].ContainsKey(coord.Y))
-                {
-                    return TheWholeMap[coord.X][coord.Y];
-                }
-            }
-            return null;
-        }
+
 
         public class NumberGroup
         {
@@ -162,25 +134,128 @@ namespace AdventOfCode2023.App.Days
             }
         }
 
-        /// <summary>
-        /// A coordinate on the grid
-        /// X is horizontal, Y is vertical
-        /// All cooridnates being from the top left and go down and right
-        /// </summary>
-        public class Coordinate
+
+        public string GetContentFromAllAdjecentCoords(NumberGroup numberGroup)
         {
-            public int X;
-            public int Y;
-            public string Content = "";
+            string content = "";
+            foreach (Coordinate coord in numberGroup.AdjacentCoords)
+            {
+                content += GetContentFromCoord(coord);
+            }
+            return content;
+        }
+
+        public string? GetContentFromCoord(Coordinate coord)
+        {
+            if (TheWholeMap.ContainsKey(coord.X))
+            {
+                if (TheWholeMap[coord.X].ContainsKey(coord.Y))
+                {
+                    return TheWholeMap[coord.X][coord.Y];
+                }
+            }
+            return null;
+        }
+
+
+
+        /// <summary>
+        /// Part1 Probblem - Does the content of the adjacent coords contain a part? 
+        /// </summary>
+        /// <param name="AdjacenContent"></param>
+        /// <returns></returns>
+        private bool DoesItContainAPart(string AdjacenContent)
+        {
+            AdjacenContent = Regex.Replace(AdjacenContent, @"\d+|\.+| +", "").Trim();
+            Functions.DebugPrint("Filtered AdjacenContent : '" + AdjacenContent + "' Length: " + AdjacenContent.Length);
+            return !(AdjacenContent.Length == 0);
+        }
+
+        public override void Part1()
+        {
+            int TotalSumOfParts = 0;
+            foreach (NumberGroup numberGroup in AllNumberGroups)
+            {
+                string Content = GetContentFromAllAdjecentCoords(numberGroup);
+                bool IsPart = DoesItContainAPart(Content);
+                Functions.DebugPrint("Number " + numberGroup.Number + " at " + numberGroup.StartingCoordinate.X + "," + numberGroup.StartingCoordinate.Y + " has content: " + Content + " Is there a Part here? : " + IsPart.ToString());
+                Functions.DebugPrint("###\n");
+                if (IsPart)
+                {
+                    TotalSumOfParts += numberGroup.Number;
+                }
+            }
+            Functions.PrintResult(TotalSumOfParts.ToString(), 3, 1);
+            //409423 is too low
+            //556057 is right!
+        }
+
+        public Coordinate? DowWeHaveGearAdjecent(NumberGroup numberGroup)
+        {
+            foreach (Coordinate coord in numberGroup.AdjacentCoords)
+            {
+                string? content = GetContentFromCoord(coord);
+                if (content == null)
+                {
+                    continue;
+                }
+                if (content == "*")
+                {
+                    return coord;
+                }
+            }
+            return null;
         }
 
 
         public override void Part2()
         {
+            int TotalSumOfPartsWithGears = 0;
+            AddAllNumbersInGear();
+            foreach (KeyValuePair<int, Dictionary<int, List<NumberGroup>>> gearX in AllGears)
+            {
+                foreach (KeyValuePair<int, List<NumberGroup>> gearY in gearX.Value)
+                {
+                    if (gearY.Value.Count != 2)
+                    {
+                        Functions.DebugPrint("Gear at " + gearX.Key + "," + gearY.Key + " has " + gearY.Value.Count + " gears - skipping");
+                        continue;
+                    }
+                    TotalSumOfPartsWithGears += gearY.Value[0].Number * gearY.Value[1].Number;
+                    Functions.DebugPrint("###\n");
+                }
+            }
+
+            Functions.PrintResult(TotalSumOfPartsWithGears.ToString(), 3, 2);
+            //82824352 Correct
 
 
         }
-    }
 
+
+        private void AddAllNumbersInGear()
+        {
+            foreach (NumberGroup numberGroup in AllNumberGroups)
+            {
+                Coordinate? GearFound = DowWeHaveGearAdjecent(numberGroup);
+                if (GearFound == null)
+                {
+                    continue;
+                }
+
+                if (!AllGears.ContainsKey(GearFound.X))
+                {
+                    AllGears.Add(GearFound.X, new Dictionary<int, List<NumberGroup>>());
+                }
+                if (!AllGears[GearFound.X].ContainsKey(GearFound.Y))
+                {
+                    AllGears[GearFound.X].Add(GearFound.Y, new List<NumberGroup>());
+                }
+
+                AllGears[GearFound.X][GearFound.Y].Add(numberGroup);
+            }
+
+        }
+    }
 
 }
