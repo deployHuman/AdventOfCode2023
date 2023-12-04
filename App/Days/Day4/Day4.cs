9ab8f9010b29fab1a064ba387b87054da2d3e41c
Gabriel Trosell <gabriel.trosell@gmail.com>
2023-12-04 11:41:18 +0100

    WIP

--- App/Days/Day4/Day4.cs
+++ App/Days/Day4/Day4.cs
@@ -12,7 +12,9 @@ namespace AdventOfCode2023.App.Days
             ReadInAllCards();
         }
 
-        Dictionary<int, ScratchCard> AllCards = new();
+        Dictionary<int, List<ScratchCard>> AllCards = new();
+
+        Dictionary<int, List<ScratchCard>> OnlyWinningCards = new();
 
         public class ScratchCard()
         {
@@ -25,13 +27,17 @@ namespace AdventOfCode2023.App.Days
 
             public int Part1Score { get; set; } = 0;
 
+            public int NumberOfMatches { get; set; } = 0;
+
+            public bool HasBeenEvaluated { get; set; } = false;
+
         }
 
         //Card   1:  4 16 87 61 11 37 43 25 49 17 | 54 36 14 55 83 58 43 15 87 17 97 11 62 75 37  4 49 80 42 61 20 79 25 24 16
 
         public override void Part1()
         {
-            Functions.PrintResult(CalculateWinnings(AllCards).ToString(), 4, 1);
+            Functions.PrintResult(CalculateWinningsPart1(AllCards).ToString(), 4, 1);
             //15268 was correct
         }
 
@@ -55,44 +61,110 @@ namespace AdventOfCode2023.App.Days
 
                 card.Numbers = numbers1Array;
                 card.WinningNumbers = numbers2Array;
-                AllCards.Add(card.CardNumber, card);
+                AllCards.Add(card.CardNumber, [card]);
             }
         }
 
-        private int CalculateWinnings(Dictionary<int, ScratchCard> InputCheck)
+        private int CalculateWinningsPart1(Dictionary<int, List<ScratchCard>> InputCheck)
         {
             int TotalWinningsScore = 0;
 
             foreach (var card in InputCheck)
             {
-                int winnings = 0;
-                foreach (var number in card.Value.Numbers)
+                foreach (var number in card.Value.First().Numbers)
                 {
-                    if (card.Value.WinningNumbers.Contains(number))
+                    if (card.Value.First().WinningNumbers.Contains(number) == false)
                     {
-                        if (card.Value.Part1Score == 0)
-                        {
-                            card.Value.Part1Score = 1;
-                        }
-                        else
-                        {
-                            card.Value.Part1Score *= 2;
-                        }
+                        continue;
+                    }
+
+                    card.Value.First().NumberOfMatches++;
+
+                    if (card.Value.First().Part1Score == 0)
+                    {
+                        card.Value.First().Part1Score = 1;
+                    }
+                    else
+                    {
+                        card.Value.First().Part1Score *= 2;
                     }
                 }
-                TotalWinningsScore += card.Value.Part1Score;
-                Functions.DebugPrint("Card " + card.Key + " has " + winnings + " winning numbers");
+
+                TotalWinningsScore += card.Value.First().Part1Score;
+                Functions.DebugPrint("Card " + card.Key + " has " + card.Value.First().NumberOfMatches + " winning numbers");
             }
             return TotalWinningsScore;
-
-
         }
+
 
 
         public override void Part2()
         {
+            CopyOverAllWinningCards();
+            CreateAllCopysOfCards();
+
         }
 
+        private void CopyOverAllWinningCards()
+        {
+            OnlyWinningCards.Clear();
+            foreach (var card in AllCards)
+            {
+                if (card.Value.First().NumberOfMatches > 0)
+                {
+                    OnlyWinningCards.Add(card.Key, [card.Value.First()]);
+                }
+            }
+        }
+
+        private int CreateAllCopysOfCards()
+        {
+            int TotalDuplicatesCreated = 0;
+            Dictionary<int, List<ScratchCard>> DicToBeCopiedFrom = new();
+
+            foreach (var cards in OnlyWinningCards)
+            {
+
+                foreach (var SingleCard in cards.Value)
+                {
+
+                    DicToBeCopiedFrom.Add(SingleCard.CardNumber, [SingleCard]);
+
+                    if (SingleCard.HasBeenEvaluated == true)
+                    {
+                        continue;
+                    }
+
+                    int NumberOfCopys = SingleCard.NumberOfMatches;
+
+                    for (int i = 0; i < NumberOfCopys; i++)
+                    {
+                        ScratchCard newCard = OnlyWinningCards[SingleCard.CardNumber + i].First();
+                        newCard.HasBeenEvaluated = false;
+
+                        if (OnlyWinningCards.ContainsKey(newCard.CardNumber) == false)
+                        {
+                            OnlyWinningCards.Add(newCard.CardNumber, [newCard]);
+                        }
+                        else
+                        {
+                            OnlyWinningCards[newCard.CardNumber].Add(newCard);
+                        }
+
+                        TotalDuplicatesCreated++;
+                    }
+                    SingleCard.HasBeenEvaluated = true;
+                }
+
+
+            }
+
+            return TotalDuplicatesCreated;
+        }
+
+
     }
+
+
 
 }
